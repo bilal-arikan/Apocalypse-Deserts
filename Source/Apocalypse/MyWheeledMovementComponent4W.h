e89a55070912769d3b5b59f440be0e8090a16dc2
Bilal Arikan <bilal1993arikan@gmail.com>
2020-07-13 03:31:42 +0300

    Initial Commit

---
+++ Source/Apocalypse/MyWheeledMovementComponent4W.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Vehicles/WheeledVehicleMovementComponent4W.h"
+#include "MyWheeledMovementComponent4W.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class APOCALYPSE_API UMyWheeledMovementComponent4W : public UWheeledVehicleMovementComponent4W
+{
+	GENERATED_BODY()
+
+	
+	
+};
